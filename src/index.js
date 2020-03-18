b56a7e4979b3ea265b8a7bc3f4332c129ac3620d
nata-kostina <natakostina123@gmail.com>
2020-03-18 16:15:14 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        let min = array[0];
+        for (el of array) {
+            if (min > el)
+                min = el;
+        }
+        return min;
+    }
+    else
+        return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        let max = array[0];
+        for (el of array) {
+            if (max < el)
+                max = el;
+        }
+        return max;
+    }
+    else
+        return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        let sum = 0;
+        for (el of array) {
+            sum += el;
+        }
+        return sum / array.length;
+    }
+    else
+        return 0;
 }
